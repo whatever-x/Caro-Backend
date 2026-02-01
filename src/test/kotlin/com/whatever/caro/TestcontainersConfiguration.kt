0da2c619ae38612e2e5e5ece66d975af206287c3
Yi JoonYong <yijy001@naver.com>
2026-02-01 16:43:18 +0900

    [Chore] ci spotless 실패 체크를 위한 코드 임시 변경

--- src/test/kotlin/com/whatever/caro/TestcontainersConfiguration.kt
+++ src/test/kotlin/com/whatever/caro/TestcontainersConfiguration.kt
@@ -10,9 +10,11 @@ import org.testcontainers.utility.DockerImageName
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    fun mysqlContainer(): MySQLContainer = MySQLContainer(DockerImageName.parse("mysql:latest"))
+	@Bean
+	@ServiceConnection
+	fun mysqlContainer(): MySQLContainer {
+		return MySQLContainer(DockerImageName.parse("mysql:latest"))
+	}
 
     @Bean
     @ServiceConnection(name = "redis")
