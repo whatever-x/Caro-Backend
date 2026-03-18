cc60951fa8b11c44ff01702d2e83401e133921e8
given-dragon <yijy001@naver.com>
2026-03-18 16:56:56 +0900

    [Chore] ModuleMetadata 추가

---
+++ src/main/kotlin/com/whatever/caro/study/ModuleMetadata.kt
@@ -0,0 +1,8 @@
+package com.whatever.caro.study
+
+import org.springframework.modulith.ApplicationModule
+import org.springframework.modulith.PackageInfo
+
+@PackageInfo
+@ApplicationModule(displayName = "Study")
+class ModuleMetadata
---
+++ src/main/kotlin/com/whatever/caro/user/ModuleMetadata.kt
@@ -0,0 +1,8 @@
+package com.whatever.caro.user
+
+import org.springframework.modulith.ApplicationModule
+import org.springframework.modulith.PackageInfo
+
+@PackageInfo
+@ApplicationModule(displayName = "User")
+class ModuleMetadata
