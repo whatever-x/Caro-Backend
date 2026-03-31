90a913c116ea0899812e3550d172ce9b1c5bf709
given-dragon <yijy001@naver.com>
2026-03-31 22:59:43 +0900

    [Feat] auth & user 모듈 에러코드 추가

---
+++ src/main/kotlin/com/whatever/caro/user/UserErrorCode.kt
@@ -0,0 +1,14 @@
+package com.whatever.caro.user
+
+import com.whatever.caro.common.response.ErrorCodeSpec
+import org.springframework.http.HttpStatus
+
+enum class UserErrorCode(
+    override val code: String,
+    override val status: HttpStatus,
+    override val message: String,
+    override val messageKey: String,
+) : ErrorCodeSpec {
+    NOT_FOUND("U001", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다", "error.user.not_found"),
+    ALREADY_COMPLETED("U002", HttpStatus.CONFLICT, "이미 회원가입이 완료된 사용자입니다", "error.user.already_completed"),
+}
