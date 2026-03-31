d6eec6bd6ea88d94f8dcebbde695f223a1755334
given-dragon <yijy001@naver.com>
2026-03-31 18:31:12 +0900

    [Feat] logback & otel 통합 설정 추가

---
+++ src/main/kotlin/com/whatever/caro/common/config/OpenTelemetryLogbackConfig.kt
@@ -0,0 +1,13 @@
+package com.whatever.caro.common.config
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenTelemetryLogbackConfig(private val openTelemetry: OpenTelemetry) : InitializingBean {
+    override fun afterPropertiesSet() {
+        OpenTelemetryAppender.install(openTelemetry)
+    }
+}
