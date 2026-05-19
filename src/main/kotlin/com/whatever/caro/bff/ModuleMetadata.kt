f02381cb1bcd4b902c01e70f568ee29641f6b288
given-dragon <yijy001@naver.com>
2026-05-19 22:26:29 +0900

    [Chore] bff 모듈 임시 추가

---
+++ src/main/kotlin/com/whatever/caro/bff/ModuleMetadata.kt
@@ -0,0 +1,11 @@
+package com.whatever.caro.bff
+
+import org.springframework.modulith.ApplicationModule
+import org.springframework.modulith.PackageInfo
+
+@PackageInfo
+@ApplicationModule(
+    displayName = "BackendForFrontend",
+    allowedDependencies = ["common", "study"],
+)
+class ModuleMetadata
