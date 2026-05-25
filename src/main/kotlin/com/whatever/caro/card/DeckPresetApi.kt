42032f82e2fd3c9f8f5eb158d1da78201d4da134
given-dragon <yijy001@naver.com>
2026-05-25 18:14:40 +0900

    [Feat] DeckPresetApi 추가

---
+++ src/main/kotlin/com/whatever/caro/card/DeckPresetApi.kt
@@ -0,0 +1,26 @@
+package com.whatever.caro.card
+
+import java.math.BigDecimal
+
+interface DeckPresetApi {
+    fun getLatestDeckPresetByUser(
+        deckId: Long,
+        userId: Long,
+    ): DeckPresetDto
+}
+
+data class DeckPresetDto(
+    val id: Long,
+    val userId: Long?,
+    val name: String,
+    val newPerDay: Int,
+    val newFairInterval: Int,
+    val newEasyInterval: Int,
+    val newInitialEaseFactor: BigDecimal,
+    val reviewPerDay: Int,
+    val reviewMaxInterval: Int,
+    val lapseIntervalMultiplier: BigDecimal,
+    val lapseMinInterval: Int,
+    val leechThreshold: Int,
+    val hardBadgeThreshold: Int,
+)
