7d8315951c289e75152265a6a18f34762586fc33
Ahn-seokjoo <tjrwn0716@naver.com>
2026-06-28 00:12:16 +0900

    [Feat] 512로 변경, 불필요 코드 제거

--- src/main/resources/db/migration/user/V2__add_encrypted_email_column.sql
+++ src/main/resources/db/migration/user/V2__add_encrypted_email_column.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `users`
-    ADD COLUMN encrypted_primary_email VARCHAR(255) DEFAULT NULL NULL
+    ADD COLUMN encrypted_primary_email VARCHAR(512) NULL;
 
 ALTER TABLE `social_accounts`
-    ADD COLUMN encrypted_email VARCHAR(255) DEFAULT NULL NULL
+    ADD COLUMN encrypted_email VARCHAR(512) NULL;
